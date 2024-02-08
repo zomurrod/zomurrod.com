0d5e1a6278a36d886c7187a9bf815ff29fc5ee56
Dunia Muhammed <thenme96@gmail.com>
2024-02-08 21:58:05 +0300

    zomurrod content design

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,11 @@
 import './index.css';
+// import axios from 'axios';
+// document.getElementById('fetch-data')?.addEventListener('click', function() {
+//     axios.get('https://jsonplaceholder.typicode.com/posts/1')
+//         .then(function(response) {
+//             document.getElementById('data')!.textContent = JSON.stringify(response.data);
+//         })
+//         .catch(function(error) {
+//             console.error(error);
+//         });
+// });
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,12 @@ module.exports = {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      boxShadow: {
+        '1sm': '0 1px 4px  0 rgba(0, 0, 0, 0.05)',
+        '2sm': '0 2px 4px  0 rgba(143, 214, 148, 0.05)',
+      }
+    },
   },
   plugins: [],
 }
