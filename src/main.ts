6db12b2aa9bd6e219d744aa38ac5788a22e76189
Dunia Muhammed <thenme96@gmail.com>
2024-02-14 01:18:01 +0300

    new design

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,61 @@
 import './index.css';
-// import axios from 'axios';
-// document.getElementById('fetch-data')?.addEventListener('click', function() {
-//     axios.get('https://jsonplaceholder.typicode.com/posts/1')
-//         .then(function(response) {
-//             document.getElementById('data')!.textContent = JSON.stringify(response.data);
-//         })
-//         .catch(function(error) {
-//             console.error(error);
-//         });
-// });
+
+document.getElementById('open')?.addEventListener('click', function() {
+  const dialog = document.getElementById('dialog');
+  dialog.classList.remove('hidden', 'hide');
+  dialog.classList.add('show');
+  dialog.setAttribute('aria-modal', 'true');
+});
+
+document.getElementById('close')?.addEventListener('click', function() {
+  const dialog = document.getElementById('dialog');
+  dialog.classList.remove('show');
+  dialog.classList.add('hide');
+  setTimeout(() => dialog.classList.add('hidden'), 200);
+  dialog.setAttribute('aria-modal', 'false');
+});
+
+
+const searchTerms = ['Apple', 'Banana', 'Cherry', 'Date', 'Elderberry'];
+
+document.getElementById('search')?.addEventListener('input', function(e) {
+  const input = e.target.value;
+  const results = searchTerms.filter(term => term.toLowerCase().startsWith(input.toLowerCase()));
+
+  const resultsContainer = document.getElementById('autocomplete-results');
+  resultsContainer.classList.add('hidden');
+
+  if (results.length > 0 && input !== '') {
+    
+    resultsContainer.classList.remove('hidden');
+  }
+});
+const apha = ['ا', 'ب', 'ت', 'ث', 'ج','ح','خ',
+'د','ذ','ر','ز','س','ش','ص','ض'];
+
+function filterByAlpha(e){
+  let elements =document.getElementsByClassName('alpha-btn')
+  for (var i = 0; i < elements.length; i++) {
+    var element = elements[i];
+
+  element.classList.remove('text-zinc-50','bg-primary');
+  element.classList.add('text-zinc-600','bg-white');
+  }
+  
+  e.target.classList.remove('text-zinc-600','bg-white');
+  e.target.classList.add('text-zinc-50','bg-primary');
+   
+ } 
+apha.forEach(function(item, index) {
+  // Create a new list item
+  var liElement = document.createElement('button');
+  liElement.textContent = item;
+  liElement.classList.add('block','min-w-[45px]','w-[45px]','rounded-[10px]','shadow-1sm','h-[30px]','px-1','py-1','text-[12px]','text-zinc-600' ,'bg-white','alpha-btn');
+
+  liElement.addEventListener('click', function($event) {
+    filterByAlpha($event)
+  });
+
+  document.getElementById('alpha-filter')?.appendChild(liElement);
+});
+ 
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,11 +5,19 @@ module.exports = {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
+
     extend: {
+      colors: {
+        'primary': '#8FD694',
+        'primary-light': '#E8F6EE',
+      },
       boxShadow: {
         '1sm': '0 1px 4px  0 rgba(0, 0, 0, 0.05)',
         '2sm': '0 2px 4px  0 rgba(143, 214, 148, 0.05)',
-      }
+        '3sm': '0px 1px 2px rgba(39, 174, 96, 0.4), 0px 0px 0px 1px rgba(33, 150, 83, 0.7)',
+        '4sm': '0 1px 2px  0 rgba(0, 0, 0, 0.05)',
+      }, 
+
     },
   },
   plugins: [],
